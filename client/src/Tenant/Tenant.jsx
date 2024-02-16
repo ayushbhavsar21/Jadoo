23b75449207e88cd896e034c47e60c3f6a1e5503
Ayush Bhavsar <ayush21bhavsar@gmail.com>
2024-02-16 17:05:04 +0530

    Changed tanant page

--- client/src/Tenant/Tenant.jsx
+++ client/src/Tenant/Tenant.jsx
@@ -9,13 +9,13 @@ function Tenant() {
                     <div className='w-[8%] bg-tertiary ' ></div>
                     <img className=' w-[92%]  bg-tertiary  rounded-bl-[5vw]  ' src={img} alt="" />
                 </div>
-                <div className="absolute top-[50vh] left-[20vw]  h-[8vh] w-[50vw] rounded-full bg-gray-400  border-[2px] flex items-cente border-black ">
+                <div className="absolute top-[50vh] left-[20vw]  h-[8vh] w-[50vw] rounded-full bg-gray-400 text-black  border-[2px] flex items-cente border-black ">
                     <div className="  bg-gray-300 w-[4vw]  rounded-full p-[10px] border border-black  ">
                         <img className=' ' src={search} alt="" /></div>
                     <input
                         type="text"
-                        className=" w-[45vw] bg-transparent m-1 mr-5  pl-[5vw] "
-                        placeholder=' Search...'
+                        className=" w-[45vw] bg-gray-400 text-[22px] placeholder-black text-black m-1 mr-5  pl-[5vw] "
+                        placeholder=' Search .........'
                     />
                 </div>
 
