809c9fbd5ffea474c25db2a82c2cd3072fa196d3
B. Deewakar Rao <128729012+deewakar28@users.noreply.github.com>
2024-02-16 15:05:21 +0530

    Delete server directory

--- server/src/utils/ApiError.js
+++
@@ -1,9 +0,0 @@
-class ApiError {
-    constructor(statusCode, message) {
-        this.statusCode = statusCode;
-        this.message = message;
-        this.success = statusCode < 400 ? true : false;
-    }
-}
-
-export { ApiError };
