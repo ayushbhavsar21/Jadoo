643069c8a01c24affc556a8bd46734e040903153
Ayush Bhavsar <ayush21bhavsar@gmail.com>
2024-02-17 12:34:39 +0530

    Changed Homepage

--- client/src/section/Homesection1.jsx
+++ client/src/section/Homesection1.jsx
@@ -34,8 +34,8 @@ function Homesection1() {
         </video>
         <div className='absolute sm:top-[20vh] top-8  flex a justify-center items-start md:pt-36 pt-20 w-full  font-playfair'>
           <div className='md:w-[60%] w-[90vw] text-center   font-bold'>
-            <h1 className='font-bold md:text-[50px] sm:text-2xl text-xl mb-8  text-black font-dancing'>Find a home you will love!</h1>
-            <p className='md:text-[22px] sm:text-xl text-[15px] text-black '>"Welcome to Jadoo, where your dream home is just a click away. Discover a curated collection of properties, each tailored to fit your unique lifestyle. From cozy apartments to spacious family houses, we have options to suit every taste. Start your journey towards the perfect space with us today."</p>
+            <h1 className='font-bold md:text-[50px] sm:text-2xl text-xl mb-8  text-white drop-shadow-[0_5px_5px_rgba(0,0,0,1)]  font-serif '>Find a home you will love!</h1>
+            <p className='md:text-[22px] sm:text-xl text-[15px] text-white drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)]  font-playfair '>Welcome to Jadoo, where your dream home is just a click away. Discover a curated collection of properties, each tailored to fit your unique lifestyle. From cozy apartments to spacious family houses, we have options to suit every taste. Start your journey towards the perfect space with us today.</p>
             <a className='w-[100%] flex justify-center pt-5 '  >
               <h1 className='bg-black md:px-10 md:py-5 px-6 py-3 rounded-2xl  font-bold md:text-xl sm:text-2xl text-md   text-white '>GET STARTED!</h1>
             </a>
