655f065f47e815f374667dce73a98f0f64c66b18
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-04-22 23:53:17 +0530

    minor changes

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,6 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const baseURL = process.env.BASE_URL || `http://localhost:${port}`
 
 app.use(express.json({limit: "16kb"}));
 app.use(bodyParser.urlencoded({extended: true}))
@@ -24,4 +23,4 @@ import propertyRouter from './routes/property.routes.js';
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/property', propertyRouter);
 
-export { app, port, baseURL };
+export { app, port};
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import connectDB from "../src/db/index.js";
 import dotenv from "dotenv";
-import { app, port, baseURL } from "./app.js";
+import { app, port } from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -9,7 +9,7 @@ dotenv.config({
 connectDB()
 .then(()=>{
     app.listen(port , ()=>{
-        console.log(`Server is running at: ${baseURL}`)
+        console.log(`Server is running at: http://localhost:${port}`)
     })
 })
 .catch((error)=>{
