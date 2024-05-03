6084a59c8b64ce2022fcfa18e5cb9e98dfb98b28
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-05-03 16:21:13 +0530

    vercel

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json({limit: "16kb"}));
 app.use(bodyParser.urlencoded({extended: true}))
